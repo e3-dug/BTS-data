0ff2991899b414be49f14792dd27982fb8e66f99
Dan Aas <dan@ethree.com>
2017-11-21 18:41:09 -0800

    add national version

--- bio_pivot_example.py
+++ bio_pivot_example.py
@@ -10,17 +10,24 @@ bts_raw = pd.read_csv('billionton_state_download.csv', low_memory = False)
 ## only keep wastes, not cash crops
 bts_raw = bts_raw[bts_raw['Production Unit'] == 'dt']
 
-## columnar pivot table, to match example from Tory
-bts_national = pd.pivot_table(bts_raw, index = ['State', 'Year','Feedstock'], columns = ['Scenario','Resource Category',  'Biomass Price'],
+## columnar pivot table, with state index
+bts_states = pd.pivot_table(bts_raw, index = ['State', 'Year','Feedstock'], columns = ['Scenario','Resource Category',  'Biomass Price'],
+                              values = 'Production', aggfunc = np.sum)
+
+## pivot table to match Tory data
+bts_national = pd.pivot_table(bts_raw, index = ['Year','Feedstock'], columns = ['Scenario','Resource Category',  'Biomass Price'],
                               values = 'Production', aggfunc = np.sum)
 
 ## select cases
 scenario_select = ['3% yield inc.', 'High housing, high energy demands',
                   'Wastes and other residues']
 
-bts_high = bts_national[scenario_select]
+bts_state_high = bts_states[scenario_select]
+bts_national_high = bts_national[scenario_select]
 
 ## drop unncessary level from MultiIndex
-bts_high.columns = bts_high.columns.droplevel()
+bts_state_high.columns = bts_state_high.columns.droplevel()
+bts_national_high.columns = bts_national_high.columns.droplevel()
 
-bts_high.to_csv('bts_piv.csv')
+bts_state_high.to_csv('bts_states_curves.csv')
+bts_national_high.to_csv('bts_national_curves.csv')
