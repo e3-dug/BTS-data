c94d0dd0427b160e265700fee172a148844235fc
Dan Aas <dan@ethree.com>
2017-11-21 17:12:25 -0800

    pivot table script

---
+++ bio_pivot_example.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import os
+import numpy as np
+
+## bone dry tonnes per tonne
+bdt_t = 0.85
+
+## bushels corn per tonne
+bu_t = 39.3862
+
+## pounds per tonne
+lb_t = 2204.62
+
+## from Doug's spreadsheet
+bdt_conversions = {'Barley' : 0.0308,
+                   'Corn' : 0.0237,
+                   'Oats' : 0.0275,
+                   'Sorghum' : 0.0241,
+                   'Soybeans' : 0.0241, ## copied from Sorgum
+                   'Wheat' : 0.0389}
+
+
+
+'''
+Converts feekstocks to bdt
+probably not right
+'''
+
+def converter(row):
+    if row['Production Unit'] == 'dt':
+        return(row['Production'])
+    elif row['Production Unit'] == 'lb':
+        return(row['Production'] / lb_t * bdt_t)
+    elif row['Production Unit'] == 'bu':
+        return(row['Production'] * bdt_conversions[row['Feedstock']])
+    else:
+        return(None)
+
+bts_raw['Production_bdt'] = bts_raw.apply(converter, axis = 1)
+
+## columnar pivot table
+bts_national = pd.pivot_table(bts_raw, index = ['Year'], columns = ['Scenario','Resource Category', 'Feedstock', 'Biomass Price'],
+                              values = 'Production_bdt', aggfunc = np.sum)
