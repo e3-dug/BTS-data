9b7cc5ea07875db510c219593b0b3f26ccf8c6e7
Dan Aas <dan@ethree.com>
2017-11-21 18:28:37 -0800

    generate supply curves

--- bio_pivot_example.py
+++ bio_pivot_example.py
@@ -2,47 +2,25 @@ import pandas as pd
 import os
 import numpy as np
 
-## bone dry tonnes per tonne
-bdt_t = 0.85
+os.chdir(r'S:\ZacharySubins_Documents\NYSERDA 100%\BTS Data Download')
 
-## bushels corn per tonne
-bu_t = 39.3862
+## read csv into memory, force pandas to read in mixed columns
+bts_raw = pd.read_csv('billionton_state_download.csv', low_memory = False)
 
-## pounds per tonne
-lb_t = 2204.62
+## only keep wastes, not cash crops
+bts_raw = bts_raw[bts_raw['Production Unit'] == 'dt']
 
-## from Doug's spreadsheet
-bdt_conversions = {'Barley' : 0.0308,
-                   'Corn' : 0.0237,
-                   'Oats' : 0.0275,
-                   'Sorghum' : 0.0241,
-                   'Soybeans' : 0.0241, ## copied from Sorgum
-                   'Wheat' : 0.0389}
+## columnar pivot table, to match example from Tory
+bts_national = pd.pivot_table(bts_raw, index = ['State', 'Year','Feedstock'], columns = ['Scenario','Resource Category',  'Biomass Price'],
+                              values = 'Production', aggfunc = np.sum)
 
-
-
-'''
-Converts feekstocks to bdt
-probably not right
-'''
-
-def converter(row):
-    if row['Production Unit'] == 'dt':
-        return(row['Production'])
-    elif row['Production Unit'] == 'lb':
-        return(row['Production'] / lb_t * bdt_t)
-    elif row['Production Unit'] == 'bu':
-        return(row['Production'] * bdt_conversions[row['Feedstock']])
-    else:
-        return(None)
-
-bts_raw['Production_bdt'] = bts_raw.apply(converter, axis = 1)
-
-## columnar pivot table
-bts_national = pd.pivot_table(bts_raw, index = ['Year'], columns = ['Scenario','Resource Category', 'Feedstock', 'Biomass Price'],
-                              values = 'Production_bdt', aggfunc = np.sum)
-
+## select cases
 scenario_select = ['3% yield inc.', 'High housing, high energy demands',
                   'Wastes and other residues']
 
-bts_curves = bts_national[scenario_select]
+bts_high = bts_national[scenario_select]
+
+## drop unncessary level from MultiIndex
+bts_high.columns = bts_high.columns.droplevel()
+
+bts_high.to_csv('bts_piv.csv')
