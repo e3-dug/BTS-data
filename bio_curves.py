684f18b109e4fcc60f206feb986dbce3dd11c87e
Dan Aas <dan@ethree.com>
2017-11-22 16:49:29 -0800

    include all scenarios

--- bio_curves.py
+++ bio_curves.py
@@ -18,16 +18,9 @@ bts_states = pd.pivot_table(bts_raw, index = ['State', 'Year','Feedstock'], colu
 bts_national = pd.pivot_table(bts_raw, index = ['Year','Feedstock'], columns = ['Scenario','Resource Category',  'Biomass Price'],
                               values = 'Production', aggfunc = np.sum)
 
-## select cases
-scenario_select = ['3% yield inc.', 'High housing, high energy demands',
-                  'Wastes and other residues']
-
-bts_state_high = bts_states[scenario_select]
-bts_national_high = bts_national[scenario_select]
-
 ## drop unncessary level from MultiIndex
-bts_state_high.columns = bts_state_high.columns.droplevel()
-bts_national_high.columns = bts_national_high.columns.droplevel()
+bts_states.columns = bts_states.columns.droplevel()
+bts_national.columns = bts_national.columns.droplevel()
 
-bts_state_high.to_csv('bts_states_curves.csv')
-bts_national_high.to_csv('bts_national_curves.csv')
+bts_states.to_csv('bts_states_curves.csv')
+bts_national.to_csv('bts_national_curves.csv')
