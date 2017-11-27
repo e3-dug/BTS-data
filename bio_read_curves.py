7792b1467e98422f272b1aacc42e8dd623141bf2
Dan Aas <dan@ethree.com>
2017-11-27 15:54:21 -0800

    convert to class

--- bio_read_curves.py
+++ bio_read_curves.py
@@ -1,20 +1,41 @@
 import pandas as pd
 import os
-import numpy as np
+import numpy as np  #delete if not used in deaccumlate
 
 os.chdir(r'S:\ZacharySubins_Documents\NYSERDA 100%\BTS Data Download\curves')
 
 ## reads generated curves back as multi-index w/ scenario selection
-def read_curves(file_type, path, scenario = ['3% yield inc.', 'High housing, high energy demands', 'Wastes and other residues']):
-    '''Reads csv output as pandas MultiIndex.
-    User provides file national or state file type, path and scenario
-    '''
-    if file_type == 'state':
-        frame = pd.read_csv(path, index_col=[0,1,2,3], header = [0,1,2])
-        frame = frame[scenario]
-    elif file_type == 'national':
-        frame = pd.read_csv(path, index_col = [0,1,2], header = [0,1,2])
-        frame = frame[scenario]
-    else:
-        print('bad scenario entry: enter state or national')
-    return(frame)
+class ParseCurves:
+    '''class for bdt reading methods'''
+    def __init__(self, file_type, path, scenario):
+        self.file_type = file_type.lower()
+        self.path = path
+        self.scenario = scenario
+
+
+    def read_bdt_curves(self):
+        '''reads supply curve data as bdt'''
+        if self.file_type == 'state':
+            self.frame = pd.read_csv(self.path, index_col=[0,1,2], header = [0,1,2])
+            self.frame = self.frame[self.scenario]
+        elif self.file_type == 'national':
+            self.frame = pd.read_csv(self.path, index_col = [0,1], header = [0,1,2])
+            self.frame = self.frame[self.scenario]
+        else:
+            print('bad entry: enter state or national')
+        return(self.frame)
+
+
+    def read_energy_curves(self):
+        if self.file_type == 'state':
+            frame = pd.read_csv(self.path, index_col=[0,1,2,3], header = [0,1,2,3])
+            #frame = frame[scenario]
+        elif self.file_type == 'national':
+            frame = pd.read_csv(self.path, index_col = [0,1,2], header = [0,1,2,3])
+            #frame = frame[scenario]
+        else:
+            print('bad entry: enter state or national')
+        return(frame)
+
+    ## add method to slice to year/ scenario
+    ## add method to deaccumulated once we figure that out...
